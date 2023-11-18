285295df4aa509473bf770a8e24749da47f37fd0
pmk1111 <kingpmk104@naver.com>
2023-11-18 22:25:41 +0900

    로그인 실패 관련 js 추가 및 관련 항목 수정

--- ingstagram/src/main/resources/static/assets/js/ingstagram.js
+++ ingstagram/src/main/resources/static/assets/js/ingstagram.js
@@ -1,24 +1,49 @@
-$(document).ready(function(){
-    // Set the initial image to show
-    var currentImage = 0;
-    $('.slide-img').eq(currentImage).css('z-index', '2');
+$(document).ready(function () {
+	$('.logo-text').click(function () {
+		let hostIndex = location.href.indexOf(location.host) + location.host.length;
+		let contextPath = location.href.substring(hostIndex, location.href.indexOf('/', hostIndex + 1));
+		location.href = contextPath + "/"
+	});
 
-    // Function to show the next image
-    function showNextImage() {
-        var previousImage = currentImage;
-        currentImage = (currentImage + 1) % $('.slide-img').length;
+	// 슬라이드 이미지
+	var currentImage = 0;
+	$('.slide-img').eq(currentImage).css('z-index', '2');
 
-        // Set z-index for the current and previous images
-        $('.slide-img').css('z-index', '1');
-        $('.slide-img').eq(currentImage).css('z-index', '2');
+	function showNextImage() {
+		var previousImage = currentImage;
+		currentImage = (currentImage + 1) % $('.slide-img').length;
 
-        // Fade out the previous image
-        $('.slide-img').eq(previousImage).fadeOut(2000);
+		$('.slide-img').css('z-index', '1');
+		$('.slide-img').eq(currentImage).css('z-index', '2');
 
-        // Show the next image after fade-out
-        $('.slide-img').eq(currentImage).fadeIn(2000);
-    }
+		$('.slide-img').eq(previousImage).fadeOut(2000);
+		$('.slide-img').eq(currentImage).fadeIn(2000);
+	}
+	setInterval(showNextImage, 5000);
 
-    // Call the showNextImage function every 4 seconds
-    setInterval(showNextImage, 5000);
+	// id, pw 갯수 유효성 검사
+	function updateSubmitButton() {
+		let idContent = $('.id').val();
+		let pwContent = $('.password').val();
+
+		let idContentLength = idContent.length;
+		let pwContentLength = pwContent.length;
+
+		if (pwContentLength >= 6 && idContentLength >= 1) {
+			$('.submit-btn').addClass('active');
+
+			//로그인 버튼 클릭 시, 실패 텍스트 표시(임시)
+			$('.submit-btn').click(function () {
+				$('.login-fail-txt').css('display', 'block');
+			});
+		} else {
+			$('.submit-btn').removeClass('active');
+		}
+	}
+
+	$('.id, .password').keyup(function () {
+		updateSubmitButton();
+	});
+
+
 });
